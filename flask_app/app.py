f0237d61256b7606b3a6570fa175a046c3029a79
Yee Chean Chang <145558346+LeafMonarch@users.noreply.github.com>
2024-11-06 14:36:37 +0000

    Refactor: app.py
    Added explanation, learnmore and login route, as well as modified main guard/entry point.

--- flask_app/app.py
+++ flask_app/app.py
@@ -7,9 +7,28 @@ import time
 
 app = Flask(__name__)
 
+#Home page
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#Cradle Care explanation page
+@app.route('/explanation')
+def explanation():
+    return render_template('explanation.html')
+
+#Cradle Care learn more page
+@app.route('/learnmore')
+def learnmore():
+    return render_template('learnmore.html')
+
+#Google Login page - Will be implemented later
+@app.route('/login')
+def login():
+    return render_template('login.html') 
+
+
+
 
 # Route to send alerts
 @app.route('/alert', methods=['POST'])
@@ -18,4 +37,9 @@ def alert():
 
     
 if __name__ == '__main__':
-    app.run(debug=True, host='')
+    try:
+        app.run(host='0.0.0.0', port=5000)
+    finally:
+        GPIO.cleanup()  # Clean up GPIO on exit
+
+
